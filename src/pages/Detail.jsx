c16c33111198312c04d602d3b2a622c28ddb5ab5
Yeomhyewon <duagpdnjs1@naver.com>
2023-11-23 12:57:03 +0900

    todoList Redux

--- src/pages/Detail.jsx
+++ src/pages/Detail.jsx
@@ -1,5 +1,49 @@
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate, useParams } from "react-router-dom";
+import { deleteTodo } from "../redux/modules/todos";
+
 const Detail = () => {
-  return <div></div>;
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+  const { id } = useParams();
+  const todo = useSelector((state) => state.todos);
+
+  const todos = todo.filter((t) => t.id === id);
+
+  const clickDeleteList = (index) => {
+    const deletedList = todos.filter((t) => t !== todos[index]);
+
+    dispatch(deleteTodo(deletedList));
+    navigate(-1);
+  };
+
+  const clickHome = () => {
+    return navigate(-1);
+  };
+  return (
+    <div>
+      {todos.map((t, index) => {
+        console.log(t.isDone);
+        return (
+          <>
+            <button onClick={clickHome}>이전 화면으로</button>
+            <div key={t.id}>
+              <div>제목 : {t.title}</div>
+              <div>내용 : {t.body}</div>
+              <div>완료 여부 : {String(t.isDone)}</div>
+            </div>
+            <button
+              onClick={() => {
+                clickDeleteList(index);
+              }}
+            >
+              삭제
+            </button>
+          </>
+        );
+      })}
+    </div>
+  );
 };
 
 export default Detail;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,132 @@
-import React from "react";
+import React, { useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import shortid from "shortid";
+import { addTodo, deleteTodo, switchTodo } from "../redux/modules/todos";
+import { Link } from "react-router-dom";
+import styled from "styled-components";
 
 const Home = () => {
-  return <div></div>;
+  const todos = useSelector((state) => state.todos);
+
+  const dispatch = useDispatch();
+
+  const [title, setTitle] = useState("");
+  const [body, setBody] = useState("");
+
+  const titleOnChange = (e) => setTitle(e.target.value);
+  const bodyOnChange = (e) => setBody(e.target.value);
+
+  const clickAddList = () => {
+    const newList = {
+      id: shortid.generate(),
+      title,
+      body,
+      isDone: false,
+    };
+    dispatch(addTodo(newList));
+    setBody("");
+    setTitle("");
+  };
+
+  const clickDeleteList = (index) => {
+    const deletedList = todos.filter((t) => t !== todos[index]);
+
+    dispatch(deleteTodo(deletedList));
+  };
+
+  const clickSwitchList = (index) => {
+    todos[index].isDone = !todos[index].isDone;
+
+    dispatch(switchTodo([...todos]));
+  };
+
+  return (
+    <>
+      <div>
+        <div>
+          제목
+          <input value={title} onChange={titleOnChange} />
+          내용
+          <input value={body} onChange={bodyOnChange} />
+          <button onClick={clickAddList}>등록</button>
+        </div>
+        <div>
+          할일목록
+          <div>
+            {todos
+              .filter((t) => t.isDone === false)
+              .map((t, index) => {
+                return (
+                  <Stdiv key={t.id}>
+                    <div>
+                      <Link to={`/${t.id}`}>
+                        <div>{t.title}</div>
+                        <div>{t.body}</div>
+                      </Link>
+                      <button
+                        onClick={() => {
+                          clickDeleteList(index);
+                        }}
+                      >
+                        삭제
+                      </button>
+                      <button
+                        onClick={() => {
+                          clickSwitchList(index);
+                        }}
+                      >
+                        완료
+                      </button>
+                    </div>
+                  </Stdiv>
+                );
+              })}
+          </div>
+        </div>
+      </div>
+      <div>
+        완료 목록
+        <div>
+          {todos
+            .filter((t) => t.isDone === true)
+            .map((t, index) => {
+              return (
+                <Stdiv key={t.id}>
+                  <Link to={`/${t.id}`}>
+                    <div>{t.title}</div>
+                    <div>{t.body}</div>
+                  </Link>
+                  <button
+                    onClick={() => {
+                      clickDeleteList(index);
+                    }}
+                  >
+                    삭제
+                  </button>
+                  <button
+                    onClick={() => {
+                      clickSwitchList(index);
+                    }}
+                  >
+                    취소
+                  </button>
+                </Stdiv>
+              );
+            })}
+        </div>
+      </div>
+    </>
+  );
 };
+
+const Stdiv = styled.div`
+  background-color: black;
+  width: 200px;
+  height: 200px;
+
+  div {
+    background-color: palegoldenrod;
+  }
+`;
 
 export default Home;
